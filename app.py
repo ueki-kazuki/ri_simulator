e1ea93912f5a8f68d0c6acd98287fd8949c1d89f
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2018-12-07 15:18:06 +0900

    write comments.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+EC2 reserved instances applied simulator
+"""
+
 import boto3
 import pprint
 
@@ -9,6 +13,14 @@ ec2 = boto3.client('ec2')
 
 
 class EC2ReservedInstanceSimulator:
+    """
+    Applied Simulator
+
+    1. set_ec2
+    2. set_ri
+    3. simulate
+    4. list_(un)match_ec2 or ri
+    """
     def __init__(self):
         self.ec2_instances = []
         self.reserved_instances = []
@@ -17,12 +29,22 @@ class EC2ReservedInstanceSimulator:
         self.unmatch_ri = []
 
     def set_ec2(self, ec2_instances):
+        """
+        set ec2 instances
+        """
         self.ec2_instances = ec2_instances
 
     def set_ri(self, reserved_instances):
+        """
+        set ec2 reserved instances
+        """
         self.reserved_instances = reserved_instances
 
     def apply_ri(self, match_ri):
+        """
+        Subtract RI count
+        When ri count to be 0 then delete the instance item.
+        """
         i = 0
         while True:
             ri = self.reserved_instances[i]
@@ -36,6 +58,14 @@ class EC2ReservedInstanceSimulator:
             i += 1
 
     def simulate(self):
+        """
+        Simulate instance applied
+
+        applied when
+        - EC2 instance is running
+        - Same instance type
+        - Same platform
+        """
         while len(self.ec2_instances) > 0:
             i = self.ec2_instances.pop(0)
 
@@ -51,6 +81,9 @@ class EC2ReservedInstanceSimulator:
         self.unmatch_ri = self.reserved_instances
 
     def match_by_instance_state(self, instance, ri_instances):
+        """
+        return ri instances when target ec2 state is runnning
+        """
         if type(ri_instances) is not list:
             ri_instances = [ri_instances]
 
@@ -58,6 +91,9 @@ class EC2ReservedInstanceSimulator:
             return ri_instances
 
     def match_by_instance_type(self, instance, ri_instances):
+        """
+        return ri instance that instance type is same for target ec2
+        """
         if type(ri_instances) is not list:
             ri_instances = [ri_instances]
 
@@ -68,6 +104,9 @@ class EC2ReservedInstanceSimulator:
                 return r
 
     def match_by_platform(self, instance, ri_instances):
+        """
+        return ri instance that platform is same for target ec2
+        """
         if type(ri_instances) != 'array':
             ri_instances = [ri_instances]
 
@@ -78,12 +117,24 @@ class EC2ReservedInstanceSimulator:
                 return r
 
     def list_match_ec2(self):
+        """
+        return matched ec2 instances.
+        CALL after "simulate"
+        """
         return sorted(self.match_ec2, key=lambda k: k['Platform']+k['InstanceType']+k['Name'])
 
     def list_unmatch_ec2(self):
+        """
+        return not matched ec2 instances.
+        CALL after "simulate"
+        """
         return sorted(self.unmatch_ec2, key=lambda k: k['Platform']+k['InstanceType']+k['Name'])
 
     def list_unmatch_ri(self):
+        """
+        return not matched but purchased reserved instances.
+        CALL after "simulate"
+        """
         return self.unmatch_ri
 
 
@@ -92,9 +143,13 @@ def __dump_list(l):
 
 
 def list_ri():
+    """
+    gather reserved instances.
+    """
     filters = [{'Name': 'state', 'Values': ['active']}]
     response = ec2.describe_reserved_instances(Filters=filters)
 
+    # if ProductDescription is empty set as Linux/UNIX
     lst = response['ReservedInstances']
     for r in lst:
         r['ProductDescription'] = r.get('ProductDescription', 'Linux/UNIX')
@@ -102,6 +157,9 @@ def list_ri():
 
 
 def list_ec2():
+    """
+    gather ec2 instances.
+    """
     filters = []
     response = ec2.describe_instances(Filters=filters)
 
@@ -109,6 +167,8 @@ def list_ec2():
     for r in response['Reservations']:
         lst += r['Instances']
 
+    # Set Name attribute from "Name" tag value
+    # if Platform is empty set as Linux/UNIX
     for r in lst:
         name_tag = [x['Value'] for x in r['Tags'] if x['Key'] == 'Name']
         name = name_tag[0] if len(name_tag) else ''
@@ -121,6 +181,9 @@ def list_ec2():
 
 
 def handler(event, context):
+    """
+    lambda handler function
+    """
     ri_instances = list_ri()
     ec2_instances = list_ec2()
 
@@ -129,7 +192,7 @@ def handler(event, context):
     simulator.set_ri(ri_instances)
     simulator.simulate()
 
-    print("=== RI適用済インスタンス ===")
+    print("=== RI covered instances ===")
     for i in simulator.list_match_ec2():
         print("{Name:20s} {InstanceType:12s} {Platform:10s} {InstanceId:20s} {State}".format(
             InstanceId=i['InstanceId'],
@@ -139,7 +202,7 @@ def handler(event, context):
             State=i['State']['Name'] if i['State']['Name'] != 'running' else '',
         ))
 
-    print("=== RI未適用インスタンス ===")
+    print("=== RI *NOT* covered instances ===")
     for i in simulator.list_unmatch_ec2():
         print("{Name:20s} {InstanceType:12s} {Platform:10s} {InstanceId:20s} {State}".format(
             InstanceId=i['InstanceId'],
@@ -149,7 +212,7 @@ def handler(event, context):
             State=i['State']['Name'] if i['State']['Name'] != 'running' else '',
         ))
 
-    print("=== 余分RI ===")
+    print("=== Purchased but not applied RI ===")
     for i in simulator.list_unmatch_ri():
         print("{Name:20s} {InstanceType:12s} {Platform:10s} {OfferingClass:12s} {Quantity:3d} {End}".format(
             Name='',
